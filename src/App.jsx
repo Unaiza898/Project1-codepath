c09332c888dfb42e8e578a375f46b901a912528f
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2023-02-28 12:18:24 -0500

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Calendar from './components/Calendar'
 import Cards from './components/cards'
+import seychelles from '../src/assets/seychelles.jpg'
 import leka from '../src/assets/leka.jpg'
 import bora from '../src/assets/bora.jpg'
 import europe from  '../src/assets/europe.jpg'
-
+import china from '../src/assets/china.jpg'
+import norway from '../src/assets/norway.jpg'
+import paris from '../src/assets/paris.jpg'
 function App() {
 
   return (
@@ -16,23 +19,23 @@ function App() {
         
 <q className='quote'>	 Our team will take you to the moon and back for a fraction of the price</q>
 <div class="flex-container">
-<Cards name = "Swiss Alps" img = {leka}/>
+<Cards name = "norway" img = {leka}/>
 <Cards name = "bora bora" img = {bora}/>
 <Cards name = "Minneapolis"img = {europe}/>
 </div>
 
 <div class="flex-container">
-<Cards name = "Swiss Alps" img = {leka}/>
+<Cards name = "Great Wall of China" img = {china}/>
+<Cards name = "norway" img = {norway}/>
+<Cards name = "Minneapolis"img = {europe}/>
+</div>
+<div class="flex-container">
+<Cards name = "Paris" img = {paris}/>
 <Cards name = "bora bora" img = {bora}/>
 <Cards name = "Minneapolis"img = {europe}/>
 </div>
 <div class="flex-container">
-<Cards name = "Swiss Alps" img = {leka}/>
-<Cards name = "bora bora" img = {bora}/>
-<Cards name = "Minneapolis"img = {europe}/>
-</div>
-<div class="flex-container">
-<Cards name = "Swiss Alps" img = {leka}/>
+<Cards name = "seychelles" img = {seychelles}/>
 <Cards name = "bora bora" img = {bora}/>
 <Cards name = "Minneapolis"img = {europe}/>
 </div>
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-
 const Cards = (props) => {
     return(
         <div className='card'>
          <img src={props.img} className='img1'></img>
          <div className='name'> {props.name}</div>
+         <button class= "place" > View place</button>
 
         </div>
        
